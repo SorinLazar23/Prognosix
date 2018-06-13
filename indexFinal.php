e67a86e313fef40c604806247e39bdaa847a4791
LaurentiuCozma <laurentiu.cozma@info.uaic.ro>
2018-06-13 10:49:12 +0300

    Adaugare functionalitate profesor

--- indexFinal.php
+++ indexFinal.php
@@ -1,4 +1,11 @@
 <?php session_start(); ?>
+<?php
+
+const STUDENT_TYPE = '1';
+const PROFESOR_TYPE = '2';
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -51,15 +58,68 @@
 <!--  Meniu  -->
 
 <!--  Content  -->
+    <?php if(!empty($_SESSION['user'])){ ?>
+        <?php if($_SESSION['user']['type'] === STUDENT_TYPE){ ?>
+            <div class="content silver no-margin">
+                <a href="nota.php?id=1"><img class="object" src="img/BD.jpeg" alt="BD image" /></a>
+                <a href="nota.php?id=2"><img class="object" src="img/GPC.png" alt="GPC image" /></a>
+                <a href="nota.php?id=3"><img class="object" src="img/TW.jpeg" alt="TW image" /></a>
+                <a href="nota.php?id=4"><img class="object" src="img/ML.jpg" alt="ML image" /></a>
+                <a href="nota.php?id=5"><img class="object" src="img/IA.png" alt="IA image" /></a>
+                <a href="nota.php?id=6"><img class="object" src="img/Python.png" alt="Python image"/></a>
+            </div>
+        <?php }else if($_SESSION['user']['type'] === PROFESOR_TYPE){ ?>
+            <div class="content silver no-margin">
+                <form action="uploadPost.php" method="post" class="formular" enctype="multipart/form-data">
+                    <fieldset class="fieldset">
+                        <legend class="legend-top"><h2>Uploadarea fisierelor cu note</h2></legend>
+                        <p>
+                            <!-- <label for="e-mail">E-mail</label> -->
+                            <?php
+                                $conn = new mysqli("localhost", "root", "","Prognosix");
 
-	<div class="content silver no-margin">
-		<a href="nota.php"><img class="object" src="img/BD.jpeg" alt="BD image" /></a>
-		<a href="nota.php"><img class="object" src="img/GPC.png" alt="GPC image" /></a>
-		<a href="nota.php"><img class="object" src="img/TW.jpeg" alt="TW image" /></a>
-		<a href="nota.php"><img class="object" src="img/ML.jpg" alt="ML image" /></a>
-		<a href="nota.php"><img class="object" src="img/IA.png" alt="IA image" /></a>
-		<a href="nota.php"><img class="object" src="img/Python.png" alt="Python image"/></a>
-	</div>
+                                if ($conn->connect_error) {
+                                    die("Connection failed: " . $conn->connect_error);
+                                }
+
+                                $rows = [];
+
+                                if ($result = $conn->query(sprintf('SELECT * from  disciplines WHERE teacher_id="%s"', $_SESSION["user"]["id"]))) {
+                                    if ($result->num_rows > 0) {
+                                        while($row = $result->fetch_assoc()) {
+                                            $rows[] = $row;
+                                        }
+                                    }
+                                }
+
+                                $conn->close();
+                            ?>
+                            <select name="disciplina" id="disciplina">
+                                <?php foreach($rows as $row){ ?>
+                                    <option value="<?php echo $row['id']?>"><?php echo $row['name']?></option>
+                                <?php } ?>
+                            </select>
+                        </p>
+
+                        <p>
+                            <!-- <label for="parola">Parola</label> -->
+                            <input type="file" name="fisier_note" id="fisier_note" placeholder="Fisier Note" />
+                        </p>
+
+                        <p class="button-wrapper">
+                            <input type="submit" name="upload" id="upload" class="buton-formular" value="Uploadare" />
+                            <input type="reset" name="reset" id="reset" class="buton-formular" value="Resetare" />
+                        </p>
+
+                    </fieldset>
+                </form>
+            </div>
+        <?php } ?>
+    <?php } else { ?>
+        <div class="content silver no-margin">
+            <a href="loginFinal.php">Va rog sa va logati.</a>
+        </div>
+    <?php } ?>
 <!--  Content  -->
 </body>
 </html>
